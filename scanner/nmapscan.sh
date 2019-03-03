28bc272134b20c7ac6e71c1f9e23c9326a040d4e
adin <akvashi24@gmail.com>
2019-03-02 20:41:53 -0500

    Revert to bash script for scanner
    Change some formatting in the scanner's bash script to correct some JSON
    issues.  All MACS should be surrounded by quotes.

---
+++ scanner/nmapscan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo nmap -sP -n 192.168.0.0/24 | grep MAC | cut -c 14-30 > macs.json
+sed -i ':a;N;$!ba;s/\n/\",\"/g' macs.json
+sed -i '1s/^/{"macs":["/' macs.json
+cat macs.json
+echo "\"]}" >>  macs.json
+sed -i ':a;N;$!ba;s/\n//g' macs.json
+cat macs.json
+curl --header "Content-type: application/json"\
+       --request POST \
+       --data "@macs.json" \
+       --url $WH_UPDATE_ENDPOINT
+
+
