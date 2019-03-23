332978fd61edcb17a648f7f36ea9f3f4845f30a2
adin <akvashi24@gmail.com>
2019-03-23 16:46:33 -0400

    Remove debug statement
    No need to see gross mac json

--- scanner/nmapscan.sh
+++ scanner/nmapscan.sh
@@ -5,10 +5,8 @@ do
 sudo nmap -sP -n 192.168.0.0/24 | grep MAC | cut -c 14-30 > macs.json
 sed -i ':a;N;$!ba;s/\n/\",\"/g' macs.json
 sed -i '1s/^/{"macs":["/' macs.json
-cat macs.json
 echo "\"]}" >>  macs.json
 sed -i ':a;N;$!ba;s/\n//g' macs.json
-cat macs.json
 curl --header "Content-type: application/json"\
        --request POST \
        --data "@macs.json" \
